0ca74f0a8d1353dc3e208a933a685e77fd00a195
Marcus Mota <marcushcsm@gmail.com>
2020-08-19 22:39:48 +0100

    debug

--- action.js
+++ action.js
@@ -3,16 +3,18 @@ const github = require('@actions/github');
 const axios = require('axios');
 
 (async () => {
+    const request = {
+        url: 'https://api.cloudflare.com/client/v4/zones/3baa736ecb6024103e63346f0ab3712a/purge_cache', 
+        method: 'POST',
+        data: { purge_everything: true },
+        headers: {
+            'Authorization': 'Bearer ' + process.env.CLOUDFLARE_API_KEY,
+            'Content-type': 'application/json'
+        }
+    };
+
     try {
-        const response = await axios({
-            url: 'https://api.cloudflare.com/client/v4/zones/3baa736ecb6024103e63346f0ab3712a/purge_cache', 
-            method: 'POST',
-            data: { purge_everything: true },
-            headers: {
-                'Authorization': 'Bearer ' + process.env.CLOUDFLARE_SECRET_KEY,
-                'Content-type': 'application/json'
-            }
-        })
+        await axios(request)
         console.log(`Cache cleared`);
       } catch (error) {
         core.setFailed(error.response.data);
