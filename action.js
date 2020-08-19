7a2ed8b76a9e0dc305f4083bd2f2d1d8656dfd1f
Marcus Mota <marcushcsm@gmail.com>
2020-08-19 23:47:58 +0100

    add CFront cleaner feature

--- action.js
+++ action.js
@@ -1,22 +1,60 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const axios = require('axios');
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
 
 (async () => {
-    const request = {
-        url: 'https://api.cloudflare.com/client/v4/zones/3baa736ecb6024103e63346f0ab3712a/purge_cache', 
-        method: 'POST',
-        data: { purge_everything: true },
-        headers: {
-            'Authorization': 'Bearer ' + process.env.CLOUDFLARE_API_KEY,
-            'Content-type': 'application/json'
-        }
-    };
+    if(process.env.CLOUDFLARE_API_KEY) {
+        const request = {
+            url: 'https://api.cloudflare.com/client/v4/zones/3baa736ecb6024103e63346f0ab3712a/purge_cache', 
+            method: 'POST',
+            data: { purge_everything: true },
+            headers: {
+                'Authorization': 'Bearer ' + process.env.CLOUDFLARE_API_KEY,
+                'Content-type': 'application/json'
+            }
+        };
 
-    try {
-        await axios(request)
-        console.log(`Cache cleared`);
-      } catch (error) {
-        core.setFailed(error.response.data);
-      }
+        try {
+            await axios(request)
+            core.setOutput('CFlare Cache cleared')
+        } catch (error) {
+            core.setFailed(error.response.data);
+        }
+    } else {
+        core.setOutput('Missing: CLOUDFLARE_API_KEY')
+    }
+
+    if(process.env.AWS_DISTRIBUTION_ID) {
+
+        const cloudfront = new AWS.CloudFront({
+            apiVersion: '2020-05-31',
+            region: process.env.AWS_REGION,
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        });
+
+        const params = {
+            DistributionId: process.env.AWS_DISTRIBUTION_ID,
+            InvalidationBatch: { 
+              CallerReference: uuidv4(), 
+              Paths: { 
+                Quantity: '1', 
+                Items: [
+                  '/*',
+                ]
+              }
+            }
+          };
+
+        cloudfront.createInvalidation(params, function (err, data) {
+            if (err) {
+                core.setFailed(err);
+            }
+            core.setOutput('CFront Cache cleared')
+        });
+    } else {
+        core.setOutput('Missing: AWS_DISTRIBUTION_ID')
+    }
+
 })()
